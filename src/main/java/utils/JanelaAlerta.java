6d220c5984b5b249cfa621ac66eae53af12fe9cd
bladerangel <bladerangel10@gmail.com>
2017-04-15 22:47:52 -0300

    finalizando projeto

--- src/main/java/utils/JanelaAlerta.java
+++ src/main/java/utils/JanelaAlerta.java
@@ -5,8 +5,10 @@ import javafx.scene.control.Alert;
 
 public class JanelaAlerta {
 
+    private Alert alert;
+
     public void janelaAlerta(String titulo, String cabecalho, String conteudo) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(titulo);
         alert.setHeaderText(cabecalho);
         alert.setContentText(conteudo);
